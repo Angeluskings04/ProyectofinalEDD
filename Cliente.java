94c83105c607d5b37b3399f4acca2615a2d634bf
unknown <castro_angel@ciencias.unam.mx>
2023-06-10 19:19:13 -0600

    50%

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,23 @@
 
 public class Cliente{
 
-}
+    private String nombre;
+    private String tipoDeCliente;
+    private String evento;
+  
+    public Cliente(String nombre, String tipoDeCliente, String evento){
+      this.nombre = nombre;
+      this.tipoDeCliente = tipoDeCliente;
+      this.evento = evento;
+    }
+  
+    public String getNombre(){
+      return nombre;
+    }
+  
+    public String getTipoDeCliente(){
+      return tipoDeCliente;
+    }
+  
+  }
+
